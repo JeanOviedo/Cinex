c614be279fc8ef4319259efe8e7fb5133b3c88cc
JeanOviedo <85713073+JeanOviedo@users.noreply.github.com>
2021-12-29 20:07:35 -0500

    Opción de favoritos agregada

--- src/Componentes/Favoritos.jsx
+++ src/Componentes/Favoritos.jsx
@@ -1,12 +1,70 @@
-import React from "react";
+import React, { Component } from "react";
 // import { connect } from "react-redux";
 // import { Link } from 'react-router-dom';
+import { connect } from "react-redux";
+import { ElEliminaFavoritos } from "../Redux/Actions.jsx";
+export class Favoritos extends Component {
+  render() {
+    return (
+      <div className="res">
+        {
+          <center>
+            <h2>Mis Peliculas Favoritas</h2>
+          </center>
+        }
+        <ul>
+          {this.props.pelicula != "" ? (
+            this.props.pelicula.map((pelicula) => {
+              return (
+                /* Aqui tienes que escribir tu codigo para mostrar la lista de peliculas */
 
-export default function Favoritos() {
-  return (
-    <div className="fav">
-      <h2>Películas Favoritas</h2>
-      <ul>{/* Aqui deberias poner tu lista de peliculas! */}</ul>
-    </div>
-  );
+                <div className="card" key={Math.random(5)}>
+                  <div>
+                    <center>
+                      <h3> {pelicula.Title}</h3>
+                      {pelicula.Poster === "N/A" ? (
+                        <img
+                          src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/495px-No-Image-Placeholder.svg.png"
+                          className="card-image"
+                          alt="..."
+                        />
+                      ) : (
+                        <img
+                          src={pelicula.Poster}
+                          className="card-image"
+                          alt="..."
+                        />
+                      )}
+                      <button
+                        className="boton"
+                        onClick={() => this.props.ElEliminaFavoritos(pelicula)}
+                      >
+                        Eliminar
+                      </button>{" "}
+                      <button className="boton">Detalles</button>
+                    </center>
+                  </div>
+                </div>
+              );
+            })
+          ) : (
+            <center>
+              <h4>No tienes Peliculas Favoritas :(</h4>
+            </center>
+          )}
+        </ul>
+      </div>
+    );
+  }
 }
+function mapStateToProps({ todasfavoritas }) {
+  return {
+    pelicula: todasfavoritas,
+  };
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    ElEliminaFavoritos: (id) => dispatch(ElEliminaFavoritos(id)),
+  };
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Favoritos);
